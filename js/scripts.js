211b6f1aa4fa2624da92e990247ede6e34254213
Jordan Hargreaves <jhargreaves231@gmail.com>
2021-01-13 20:11:04 -0800

    create the functioning js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,27 @@
+//Business Logic
+function subVariable(n1, n2){
+  return n1 - n2;
+};
 
+function sentence(input1, input2){
+  return "Your age is " + $("input#age").val() + ". Your favorite food is " + $("input#food").val() + "."; 
+};
 
-
+//UI Logic
 $(document).ready(function(){
   $("#formOne").submit(function(event) {
     event.preventDefault();
 
     const input = $("input#name").val();
+    const n1 = $("input#number1").val();
+    const n2 = $("input#number2").val();
+    const input1 = $("input#age").val();
+    const input2 = $("input#food").val();
 
     $(".person").text(input);
+    $(".subtractionResult").text(subVariable(n1, n2));
+
+    $(".longerSentence").text(sentence(input1, input2)); 
 
     $("#reveal").show();
   });
